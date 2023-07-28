a6bc64105525982d550999288cebbff3e11dcb9f
Deibson Cogo <deibsoncogo@outlook.com>
2023-07-28 16:39:20 -0300

    🗃️ feat: implementação do banco de dados

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { DatabaseModule } from '../../database/database.module'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
