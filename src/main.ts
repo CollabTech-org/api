7627c4fe49ffa4515116fcb2f1e916ec0869930f
Deibson Cogo <deibsoncogo@outlook.com>
2023-07-28 14:27:03 -0300

    ⚡️ feat: implementação da validação de dados

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 import { AppModule } from './modules/app/app.module'
+import { ValidationCustomizedPipe } from './pipes/validation.customized.pipe'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   )
+
+  app.useGlobalPipes(new ValidationCustomizedPipe())
 
   await app.listen(3000)
 }
