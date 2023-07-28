be89ecbae371e04ee18c7b8f4f6e332b5b4a3f8c
Deibson Cogo <deibsoncogo@outlook.com>
2023-07-28 16:50:45 -0300

    🔥 fix: exclusão de arquivos criado automaticamente

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { DatabaseModule } from '../../database/database.module'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
